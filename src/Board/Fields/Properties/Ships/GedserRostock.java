cf821e9eb1973cfb0045c152e09ae8dba8d49605
Tardis <Tardis@LAPTOP-3SVKESCF.eduroam.wireless.dtu.dk>
2017-01-12 13:08:29 +0100

    Working on Ships

---
+++ src/Board/Fields/Properties/Ships/GedserRostock.java
@@ -0,0 +1,5 @@
+package Board.Fields.Properties.Ships;
+
+public class GedserRostock {
+
+}
---
+++ src/Board/Fields/Properties/Ships/HelsingoerHelsingborg.java
@@ -0,0 +1,10 @@
+package Board.Fields.Properties.Ships;
+
+import Board.Fields.Properties.Property;
+import Game.Game;
+
+public class HelsingoerHelsingborg extends Ship {
+	public HelsingoerHelsingborg(Game game, String name, String description){
+		super(game, "SHIPHELSINGØR", "description", 4000);
+		}
+}
