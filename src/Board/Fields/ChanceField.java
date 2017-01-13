52b6d85b04b749754918052947a2674b83bfff80
s152695 <s152695@DTU-CZC6268C79.win.dtu.dk>
2017-01-13 11:42:33 +0100

    LidtChanceField

--- src/Board/Fields/ChanceField.java
+++ src/Board/Fields/ChanceField.java
@@ -9,5 +9,13 @@ public class ChanceField extends Field {
    
 	public ChanceField(Game game, String name, String description) {
         super(game, "ChanceField", "description)");
+        
     }
+	public void onLand(Player player){
+		super.onLand(player);
+		this.game.getCardPile().drawCard(player);
+		
+	}
+
+	
 }
