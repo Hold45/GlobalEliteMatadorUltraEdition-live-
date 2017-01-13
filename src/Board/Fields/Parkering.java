da0534eca0100b07b242e7d1bb5cc0e0cf521082
Tardis <Tardis@LAPTOP-3SVKESCF.eduroam.wireless.dtu.dk>
2017-01-13 11:01:54 +0100

    Functions with Eclipse

---
+++ src/Board/Fields/Parkering.java
@@ -0,0 +1,13 @@
+package Board.Fields;
+
+import Game.Game;
+import Owners.Player;
+
+
+public class Parkering extends Field {
+	
+   
+	public Parkering(Game game, String name, String description) {
+        super(game, "Parkering", "description)");
+    }
+}
--- src/Board/Fields/Properties/Ships/HelsingoerHelsingborg.java
+++ src/Board/Fields/Properties/Ships/HelsingoerHelsingborg.java
@@ -6,6 +6,7 @@ import Game.Game;
  * Created by JAL on 12/01/2017.
  */
 public class HelsingoerHelsingborg extends Ship {
+	
     public HelsingoerHelsingborg(Game game, String name) {
         super(game, "PropertyShipsHelsingoerHelsingborg");
     }
