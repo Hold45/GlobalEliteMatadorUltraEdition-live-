80ba24534530fd03a66c0a5dcce927c191b0c022
TheWombat13 <jal@atol.dk>
2017-01-14 12:20:55 +0100

    ship price

--- src/Board/Fields/Properties/Brewery/Brewery.java
+++ src/Board/Fields/Properties/Brewery/Brewery.java
@@ -1,6 +1,7 @@
 package Board.Fields.Properties.Brewery;
 
 import Board.Fields.Properties.Property;
+import Dice.Die;
 import Game.Game;
 
 import java.awt.*;
@@ -14,6 +15,14 @@ public abstract class Brewery extends Property {
 
     @Override
     public int getRent() {
-        return (int) Math.pow(2, this.getOwnedFriends().count()-1);
+       // return (int) Math.pow(2, this.getOwnedFriends().count()-1);
+
+        if (getOwnedFriends().count() == 1)
+        {
+            return Math.abs(100 * getGame().getCup().getSum());
+        } else {
+            return Math.abs(200 * getGame().getCup().getSum());
+        }
+
     }
 }
